0cf955353a03d7626db851f377378a9bec2688ea
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-25 20:36:35 +0300

    Creating a Web Server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Merhaba!');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Merhaba!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening!');
+});
