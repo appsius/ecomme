842c6be8df1a81c0632dc64ec000cd3e58b274e9
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-25 21:31:42 +0300

    Parsing Form Data

--- index.js
+++ index.js
@@ -5,14 +5,27 @@ const app = express();
 app.get('/', (req, res) => {
   res.send(`
   <div>
-    <form>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Password Confirmation" />
+    <form method="POST">
+      <input name="email" placeholder="Email" />
+      <input name="password" placeholder="Password" />
+      <input name="passwordConfirmation" placeholder="Password Confirmation" />
       <button>Sign Up</button>
     </form>
   </div>
   `);
+});
+
+app.post('/', (req, res) => {
+  req.on('data', data => {
+    const parsed = data.toString('utf8').split('&');
+    const formData = {};
+    for (let pair of parsed) {
+      const [key, value] = pair.split('=');
+      formData[key] = value;
+    }
+    console.log(formData);
+  });
+  res.send('Account Created!');
 });
 
 app.listen(3000, () => {
