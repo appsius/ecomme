a91a3be94329a9fb5900b5dbc0c2ac8b32718110
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-25 20:43:55 +0300

    Displaying Simple HTML

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Merhaba!');
+  res.send(`
+  <div>
+    <form>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <input type="password" placeholder="Password Confirmation" />
+      <button>Sign Up</button>
+    </form>
+  </div>
+  `);
 });
 
 app.listen(3000, () => {
