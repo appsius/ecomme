7cd2dc0a7ea1e9274a28dba71f4e4d4178c46230
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-27 06:12:23 +0300

    Fetching Session

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const usersRepo = require('./repositories/users');
 
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ['skkskskskkskskskkskskk']
+  })
+);
 
 app.get('/', (req, res) => {
   res.send(`
   <div>
+    Your id is: ${req.session.userId}
     <form method="POST">
       <input name="email" placeholder="Email" />
       <input name="password" placeholder="Password" />
@@ -17,8 +26,25 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', (req, res) => {
-  console.log(req.body);
+app.post('/', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+
+  if (existingUser) {
+    return res.send('Email in use');
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send('Sorry, you may make a typo!');
+  }
+
+  // Create a user in our repo to represent this person
+  const user = await usersRepo.create({ email, password });
+
+  // Store the id of that person inside users cookie
+  req.session.userId = user.id;
+
   res.send('Account Created!');
 });
 
