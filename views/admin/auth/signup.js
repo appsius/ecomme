2cf721b80bffabcd021c00fde85cd48c17621481
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-28 21:36:04 +0300

    Building a Layout File

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,13 +1,17 @@
+const layout = require('../layout');
+
 module.exports = ({ req }) => {
-  return `
-  <div>
-    Your id is: ${req.session.userId}
-    <form method="POST">
-      <input name="email" placeholder="Email" />
-      <input name="password" placeholder="Password" />
-      <input name="passwordConfirmation" placeholder="Password Confirmation" />
-      <button>Sign Up</button>
-    </form>
-  </div>
-  `;
+  return layout({
+    content: `
+      <div>
+        Your id is: ${req.session.userId}
+        <form method="POST">
+          <input name="email" placeholder="Email" />
+          <input name="password" placeholder="Password" />
+          <input name="passwordConfirmation" placeholder="Password Confirmation" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+  `
+  });
 };
