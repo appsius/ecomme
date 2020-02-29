534dd5869b8b9714aed54cd5a32eb4786880a768
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-29 10:31:07 +0300

    Creating the Product Repository

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const productsRepo = require('../../repositories/products');
 
 router.get('/admin/products', (req, res) => {});
 router.get('/admin/products/new', (req, res) => {});
