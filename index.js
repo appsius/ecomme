6a7c00ff4adba02adfa6cb9d0e65587d7d64c7bf
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-29 17:47:30 +0300

    Some Quick Validation

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
-    keys: ['skkskskskkskskskkskskk']
+    keys: ['']
   })
 );
 app.use(authRouter);
